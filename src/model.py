1045f1d67bba70f78464d72662a47b2e12420acf
idaneden <37079533+idaneden@users.noreply.github.com>
2020-07-22 17:48:28 +0300

    half of the website

---
+++ src/model.py
@@ -0,0 +1,46 @@
+
+class User:
+    def __init__(self,name):
+        self.__name=name
+
+    def getName(self):
+        return self.__name
+
+    
+class Room:
+    def __init__(self,id):
+        self.__members=[]
+        self.__id=id
+
+    def getId(self):
+        return self.__id
+
+    def addMember(self,user):
+        if (not self.memberExists(user.getName())):
+            self.__members.append(user)
+
+    def getMembers(self):
+        return self.__members
+
+    def memberExists(self,username):
+        for member in self.__members:
+            if member.getName() == username:
+                return True
+        return False
+
+
+rooms = {}# id : room
+
+rooms['1234'] = Room('1234')
+
+def __getRoomById(roomId):
+    return rooms[roomId]
+
+def enterGame(roomId,username):
+    room = __getRoomById(roomId)
+    if room == None:
+        raise 'room not found'
+    room.addMember(User(username))
+
+def getMembers(roomId):
+    return __getRoomById(roomId).getMembers()
---
+++ src/server.py
@@ -0,0 +1,23 @@
+import model
+from flask import Flask,render_template,request
+
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def homePage():
+    return render_template('home.html')
+
+@app.route('/join',methods=['POST'])
+def join():
+    roomId = request.form['roomId']
+    return render_template('joinRoom.html',roomid=roomId)
+
+@app.route('/enterroom',methods=['POST'])
+def enterRoom():
+    username = request.form['username']
+    roomId = request.form['roomId']
+    model.enterGame(roomId,username)
+    members = model.getMembers(roomId)
+    return render_template('room.html',username=username,members=members)
+    
